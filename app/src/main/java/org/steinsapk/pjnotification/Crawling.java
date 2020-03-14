b5baf4c89698209ab1f90e422c55fbc1fd44df46
WEED <programmer-j@naver.com>
2020-03-15 05:08:11 +0900

    Version 3.0, Change Crawling Library from HtmlUnit to Jsoup
    There are two reasons for this.
    
    First, HtmlUnit is too slow for crawling.
    For example, it takes a while to find DOM element using CSS selector and it takes so long to get a web page.
    
    Second, HtmlUnit sometimes crashes in the background task.
    I checked the logs but the crash points and the crash causes were different everytime, thus, I still do not know why exactly.
    It might be Android limits the resources for background tasks, which is insufficient for HtmlUnit.
    By the way, it worked without any problem on desktop and foreground task in Android.
    
    After change to Jsoup, it worked well and the crawling becomes faster than before.
    However, the app still uses HtmlUnit for log in, which takes some time to finish.
    For now, I think delayed background task problem is solved although I should further examine the logs to confirm.
    This is the reason that I set the version to 3.0 not 2.2.

--- app/src/main/java/org/steinsapk/pjnotification/Crawling.java
+++ app/src/main/java/org/steinsapk/pjnotification/Crawling.java
@@ -7,54 +7,45 @@ import android.util.Log;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 
-import java.net.MalformedURLException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Crawling {
-    private WebClient webClient;
-    private HtmlPage page;
+    private Map<String, String> cookies;
     private Database db;
     private Context context;
 
     public Crawling(Context context, SQLiteDatabase db) {
+        cookies = new HashMap<String, String>();
         this.context = context;
-
-        // WebClient 초기화
-        webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
-        debugLog("WebClient Created!");
-
-        // WebClient 옵션 설정 - 에러 메시지 안 나오게
-        webClient.getOptions().setThrowExceptionOnScriptError(false);
-        webClient.getOptions().setCssEnabled(false);
-        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-
-        // 비밀번호 변경 창 무시
-        webClient.getOptions().setPopupBlockerEnabled(true);
-
-        // 필드 초기화
         this.db = new Database(db);
     }
 
     public boolean login(String id, String pw) throws Exception {
+        WebClient webClient = initializeWebClient();
+
         // 로그인 페이지 연결
-        page = webClient.getPage("https://yscec.yonsei.ac.kr/login/index.php");
+        HtmlPage page = webClient.getPage("https://yscec.yonsei.ac.kr/login/index.php");
         debugLog("Get Login Page!");
 
         // 로그인 Form 객체 가져오기
@@ -73,30 +64,33 @@ public class Crawling {
         page = loginButton.click();
         debugLog("Get myPage!");
 
+        // 쿠키 저장
+        for (Cookie cookie : webClient.getCookieManager().getCookies()) {
+            cookies.put(cookie.getName(), cookie.getValue());
+        }
+
         // 로그인이 성공했는지 확인
         return page.getTitleText().equals("YSCEC");
     }
 
     // 강의 목록과 링크를 DB에 저장한다.
-    public void saveCourseList() {
-        // 디버깅용 코드 - 학기 바꾸기.
-        /*
-        try {
-            page = webClient.getPage("https://yscec.yonsei.ac.kr/my/?year=2018&term=2");
-        } catch (IOException e) {
-            debugLog(e.toString());
-        }
-        */
+    public void saveCourseList() throws IOException {
+        // My page 접속
+        Document document = Jsoup.connect("https://yscec.yonsei.ac.kr/my/").cookies(cookies).get();
+
+        // 디버깅용 코드 - 학기 바꾸기
+        //document = Jsoup.connect("https://yscec.yonsei.ac.kr/my/?year=2018&term=2").cookies(cookies).get();
+
         // 수강 변경, 철회 등을 대비해 기존 데이터 지우기
         db.clearCourse();
 
         // 모든 강의에 대해서 링크 페이지와 강의 이름 찾기
-        DomNodeList<DomNode> courseList = page.querySelectorAll("h3.coursename");
+        Elements elements = document.select("h3.coursename");
 
-        for (DomNode node : courseList) {
-            HtmlAnchor htmlAnchor = (HtmlAnchor) node.getFirstChild();
-            String courseLink = htmlAnchor.getAttribute("href");
-            String courseName = htmlAnchor.asText();
+        for (Element element : elements) {
+            Element courseElement = element.child(0);
+            String courseName = courseElement.text();
+            String courseLink = courseElement.attr("href");
 
             // DB에 추가
             db.insertCourse(courseName, courseLink);
@@ -114,26 +108,26 @@ public class Crawling {
             debugLog(courseName + " DB 저장 시작");
 
             // 강의 페이지 접속
-            HtmlPage coursePage = webClient.getPage(courseLink);
+            Document coursePage = Jsoup.connect(courseLink).cookies(cookies).get();
 
             // 게시판 링크 찾아서 들어가기
-            DomNodeList<DomNode> spanElements = coursePage.querySelectorAll(".instancename");
+            Elements spanElements = coursePage.select(".instancename");
 
-            for (int i = 0; i < spanElements.size(); i++) {
-                HtmlSpan spanElement = (HtmlSpan) spanElements.get(i);
+            for (Element spanElement : spanElements) {
+                String boardName = spanElement.text();
+                Element accessHide = spanElement.selectFirst(".accesshide");
 
-                String boardName = spanElement.asText();
-                String itemAttribute = null;
-                try {
-                    itemAttribute = spanElement.querySelector(".accesshide").asText();
-                } catch (NullPointerException e) {
-                    // accesshide가 없는 참고문헌은 무시하기.
+                if (accessHide == null) {
                     continue;
                 }
 
+                String itemAttribute = accessHide.text();
+
+
                 // 게시판만 루프 돌기
                 if (itemAttribute.equals("게시판(일반)") || itemAttribute.equals("Forum(General)")) {
-                    HtmlPage noticeListPage = spanElement.click();
+                    String noticeListLink = spanElement.parent().attr("href");
+                    Document noticeListPage = Jsoup.connect(noticeListLink).cookies(cookies).get();
 
                     // 각 공지사항에 대해서 루프 돌면서 DB에 저장하기
                     loopNotice(noticeListPage, courseName, boardName);
@@ -154,25 +148,23 @@ public class Crawling {
             debugLog(courseName + " ITEM UPDATE 시작");
 
             // 강의 페이지 접속
-            HtmlPage coursePage = webClient.getPage(courseLink);
+            Document coursePage = Jsoup.connect(courseLink).cookies(cookies).get();
 
             // Anchor 요소를 찾는다.
-            DomNodeList<DomNode> list = coursePage.querySelectorAll("ul.section.img-text a");
+            Elements anchors = coursePage.select("ul.section.img-text a");
 
-            for (int i = 1; i < list.size(); i++) {
-                // anchor 가져오기
-                HtmlAnchor anchor = (HtmlAnchor) list.get(i);
+            for (Element anchor : anchors.subList(1, anchors.size())) {
+                String itemLink = anchor.attr("href");
 
-                // itemName, itemLink, itemAttribute 가져오기
-                String itemLink = anchor.getHrefAttribute();
-                String itemAttribute = null;
-                try {
-                    itemAttribute = anchor.querySelector(".accesshide").asText();
-                } catch (NullPointerException e) {
+                Element accessHide = anchor.selectFirst(".accesshide");
+                if (accessHide == null) {
                     // accesshide가 없는 참고문헌은 무시하기.
                     continue;
                 }
-                String itemName = anchor.querySelector(".instancename").asText();
+                String itemAttribute = accessHide.text();
+
+                //String itemName = anchor.querySelector(".instancename").asText();
+                String itemName = anchor.selectFirst(".instancename").text();
                 itemName = itemName.substring(0, itemName.indexOf(itemAttribute));
                 String itemContents = "";
 
@@ -186,8 +178,8 @@ public class Crawling {
 
                 // itemAttribute가 파일이라면 접속하지 말기.
                 if (!(itemAttribute.equals("파일") || itemAttribute.equals("File"))) {
-                    HtmlPage page = webClient.getPage(itemLink);
-                    itemContents = page.asXml();
+                    Document page = Jsoup.connect(itemLink).cookies(cookies).get();
+                    itemContents = page.toString();
                 }
 
                 // DB에 넣기
@@ -217,66 +209,65 @@ public class Crawling {
         return date.getTime();
     }
 
-    private void loopNotice(HtmlPage noticeListPage, String courseName, String boardName) throws Exception {
+    private void loopNotice(Document noticeListPage, String courseName, String boardName) throws Exception {
         boolean first = true;
-        HtmlPage noticePage = null;
-        HtmlAnchor anchor;
+        Document noticePage = null;
+        Element anchor;
 
         // 순회하면서 기록하기
         while (true) {
             if (first) {
                 // class="thread-post-title"인 h1 태그를 찾는다.
-                DomNode domNode = noticeListPage.querySelector("li:not([class]) h1.thread-post-title");
+                Elements noticeTitles = noticeListPage.select("li:not([class]) h1.thread-post-title");
 
                 // 링크 얻기
-                try {
-                    anchor = (HtmlAnchor) domNode.getFirstChild();
-                } catch (NullPointerException e) {
+                anchor = noticeTitles.first();
+                if (anchor == null) {
                     debugLog("공지가 하나도 없습니다!");
                     break;
-                } catch (ClassCastException e) {
-                    debugLog("비공개 글입니다.");
+                }
+
+                anchor = anchor.selectFirst("a[onclick]");
+                if (anchor == null) {
+                    debugLog("비공개 글입니다!");
                     break;
                 }
 
                 // 공지 페이지 들어가기
-                noticePage = anchor.click();
-
-                // 다시 들어가기
-                anchor = noticePage.querySelector(".title.col-2").querySelector("a");
-                noticePage = webClient.getPage(anchor.getAttribute("href"));
+                String contentId = anchor.attr("onclick").replaceAll("\\D+","");
+                String noticePageUrl = "https://yscec.yonsei.ac.kr/mod/jinotechboard/content.php?contentId=" + contentId + "&b=&boardform=1";
+                noticePage = Jsoup.connect(noticePageUrl).cookies(cookies).get();
             } else {
                 // 그 다음 페이지 들어가기 - 마지막 tr을 선택
-                DomNode tr = noticePage.querySelector("div.table-footer-area + table > thead > tr:last-child");
+                Element tr = noticePage.selectFirst("div.table-footer-area + table > thead > tr:last-child");
 
                 // 댓글 있는 게시글
                 if (tr == null)
-                    tr = noticePage.querySelector("div.table-reply-area + table > thead > tr:last-child");
+                    tr = noticePage.selectFirst("div.table-reply-area + table > thead > tr:last-child");
 
                 // <a> 선택 후 페이지 이동
-                anchor = tr.querySelector("a");
-                try {
-                    noticePage = webClient.getPage(anchor.getAttribute("href"));
-                } catch (MalformedURLException malformedURLException) {
+                anchor = tr.selectFirst("a");
+                String nextLink = anchor.attr("href");
+
+                if (nextLink.equals("#")) {
                     // 비공개 글
                     break;
                 }
+
+                noticePage = Jsoup.connect(nextLink).cookies(cookies).get();
             }
 
             // 공지 제목, 글, 날짜, 링크 주소, 첨부 파일 얻기
-            String noticeTitle = noticePage.querySelector("span.detail-title").asText();
-            String noticeContents = noticePage.querySelector("div.detail-contents").asText();
-            String noticeDate = noticePage.querySelector("span.detail-date").asText();
-            String noticeLink = noticePage.getUrl().toString();
+            String noticeTitle = noticePage.selectFirst("span.detail-title").text();
+            String noticeContents = noticePage.selectFirst("div.detail-contents").wholeText();
+            String noticeDate = noticePage.selectFirst("span.detail-date").text();
+            String noticeLink = noticePage.baseUri();
             String attachmentFiles = "";
 
-
-            try {
-                attachmentFiles = noticePage.querySelector("ul.detail-attachment").asText();
-            } catch (NullPointerException e) {
-                // 첨부 파일이 없을 경우
+            Element attachment = noticePage.selectFirst("ul.detail-attachment");
+            if (attachment != null) {
+                attachmentFiles = attachment.text();
             }
-
 
             if (!insertNotice(courseName, noticeTitle, noticeContents, noticeDate, noticeLink, attachmentFiles, boardName))
                 break;
@@ -285,16 +276,16 @@ public class Crawling {
             Notification.makeNotification(courseName, noticeTitle, context, true, "", boardName);
 
             // 탈출 조건
-            HtmlElement htmlElement = noticePage.querySelector("div.table-footer-area + table");
+            Element htmlElement = noticePage.selectFirst("div.table-footer-area + table");
 
             // 댓글 있는 게시글
             if (htmlElement == null) {
-                htmlElement = noticePage.querySelector("div.table-reply-area + table");
+                htmlElement = noticePage.selectFirst("div.table-reply-area + table");
             }
 
             // 첫 글하고 마지막 글은 <tr> 태그가 3개이다.
             // 첫 글이자 마지막 글(글이 하나 일 때)은 <tr> 태그가 2개이다.
-            if ((!first && htmlElement.getElementsByTagName("tr").getLength() == 3) || (first && htmlElement.getElementsByTagName("tr").getLength() == 2))
+            if ((!first && htmlElement.getElementsByTag("tr").size() == 3) || (first && htmlElement.getElementsByTag("tr").size() == 2))
                 break;
 
             first = false;
@@ -313,5 +304,21 @@ public class Crawling {
     // 디버깅용 로그 출력 함수
     private static void debugLog(String log) {
         Log.e("TAG", log);
+    }
+
+    private WebClient initializeWebClient() {
+        // WebClient 초기화
+        WebClient webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
+        debugLog("WebClient Created!");
+
+        // WebClient 옵션 설정 - 에러 메시지 안 나오게
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setCssEnabled(false);
+        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+
+        // 비밀번호 변경 창 무시
+        webClient.getOptions().setPopupBlockerEnabled(true);
+
+        return webClient;
     }
 }
