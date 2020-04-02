07fdfe914f04b159e6829aa58c9c597c61c1c964
WEED <programmer-j@naver.com>
2020-04-02 19:36:12 +0900

    SQL Bug Fix
    I am not so sure but I think special course name such as "Multicore and GPU Programming" which contains a word "and" makes the SQL statement vague so that SQL parser throws an error.

--- app/src/main/java/org/steinsapk/pjnotification/ItemPage.java
+++ app/src/main/java/org/steinsapk/pjnotification/ItemPage.java
@@ -22,8 +22,8 @@ public class ItemPage extends AppCompatActivity {
         SQLiteDatabase db = Database.openDatabase(getApplicationContext());
 
         // 쿼리하기
-        String query = ("SELECT ITEMCONTENTS FROM ITEM WHERE COURSENAME='"+ courseName + "' AND ITEMNAME='" + itemName + "';").replaceAll("'", "''");
-        Cursor cursor = db.rawQuery(query, null);
+        String query = "SELECT ITEMCONTENTS FROM ITEM WHERE COURSENAME=? AND ITEMNAME=?;";
+        Cursor cursor = db.rawQuery(query, new String[] { courseName, itemName });
 
         cursor.moveToNext();
         String data = cursor.getString(0);
