7bc006f7de3c5fd53fb0be88096aabdfb0373d69
WEED <programmer-j@naver.com>
2020-04-22 02:05:14 +0900

    File Download Link
    If the item is file, there is a direct link to download below the explanations.

--- app/src/main/java/org/steinsapk/pjnotification/ItemPage.java
+++ app/src/main/java/org/steinsapk/pjnotification/ItemPage.java
@@ -22,18 +22,19 @@ public class ItemPage extends AppCompatActivity {
         SQLiteDatabase db = Database.openDatabase(getApplicationContext());
 
         // 쿼리하기
-        String query = "SELECT ITEMCONTENTS FROM ITEM WHERE COURSENAME=? AND ITEMNAME=?;";
+        String query = "SELECT ITEMCONTENTS, ITEMLINK FROM ITEM WHERE COURSENAME=? AND ITEMNAME=?;";
         Cursor cursor = db.rawQuery(query, new String[] { courseName, itemName });
 
         cursor.moveToNext();
         String data = cursor.getString(0);
+        String link = cursor.getString(1);
 
         cursor.close();
 
         WebView webView = findViewById(R.id.webView);
 
         if (data.equals("")) {
-            webView.loadData("<center><h2>파일입니다.</h2><p>뒤로 돌아가셔서 제목을 길게 누르시면 웹 브라우저로 파일을 다운로드 받으실 수 있습니다.</p></center>", "text/html", "UTF-8");
+            webView.loadData("<center><h2>파일입니다.</h2><p>뒤로 돌아가셔서 제목을 길게 누르시거나 아래의 링크를 클릭하시면 웹 브라우저로 파일을 다운로드 받으실 수 있습니다.</p><p><a href=\"" + link + "\">다운로드</a></p></center>", "text/html", "UTF-8");
         } else
             webView.loadData(data, "text/html", "UTF-8");
     }
