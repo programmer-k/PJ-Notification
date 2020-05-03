99ad21f62e61001a345b2b8a938b4adeba20f7ff
WEED <programmer-j@naver.com>
2020-05-04 05:51:06 +0900

    New Feature to View README.html
    1. Remove MarkDownViewer library because it is not good looking.
    Instead, I choose convert markdown file to html file and upload it to the server.
    
    2. Add network_security_config.xml
    Access to website that is not https is forbidden by default.
    Thus, I added this file to allow http access to my server.
    
    3. View README.html inside the application using WebView.
    This will prevent users from watching my server address.

---
+++ app/src/main/java/org/steinsapk/pjnotification/HtmlViewActivity.java
@@ -0,0 +1,18 @@
+package org.steinsapk.pjnotification;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HtmlViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_html_view);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("http://kvpn.asuscomm.com:8000/PJNotification/README.html");
+    }
+}
