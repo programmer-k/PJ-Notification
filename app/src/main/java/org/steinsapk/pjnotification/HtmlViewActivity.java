0e6a2a057a2bac5608e3f715202f96bcb9ecd85e
WEED <programmer-j@naver.com>
2020-05-04 06:22:47 +0900

    Update HtmlViewActivity to Clear Cache

--- app/src/main/java/org/steinsapk/pjnotification/HtmlViewActivity.java
+++ app/src/main/java/org/steinsapk/pjnotification/HtmlViewActivity.java
@@ -13,6 +13,7 @@ public class HtmlViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_html_view);
 
         WebView webView = findViewById(R.id.webView);
+        webView.clearCache(true);
         webView.loadUrl("http://kvpn.asuscomm.com:8000/PJNotification/README.html");
     }
 }
